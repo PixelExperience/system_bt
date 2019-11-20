83a65a25713c8549d8d0e4d18fd2ff7cf28a9662
Agung Pratama <46994407+zeuslimitless@users.noreply.github.com>
2019-11-20 19:05:45 +0700

    Update btu_hcif.cc

--- stack/btu/btu_hcif.cc
+++ stack/btu/btu_hcif.cc
@@ -726,7 +726,6 @@ constexpr uint8_t MIN_KEY_SIZE = 7;
 static void read_encryption_key_size_complete_after_encryption_change(
     uint8_t status, uint16_t handle, uint8_t key_size) {
   int ret = android::util::stats_write(
-      android::util::BLUETOOTH_CLASSIC_PAIRING_EVENT_REPORTED, "", handle,
       HCI_READ_ENCR_KEY_SIZE, HCI_COMMAND_COMPLETE_EVT, status, 0, key_size);
   if (ret < 0) {
     LOG(WARNING) << __func__ << ": failed to log encryption key size "
@@ -1695,7 +1694,6 @@ static void btu_hcif_enhanced_flush_complete_evt(void) {
 static void read_encryption_key_size_complete_after_key_refresh(
     uint8_t status, uint16_t handle, uint8_t key_size) {
   int ret = android::util::stats_write(
-      android::util::BLUETOOTH_CLASSIC_PAIRING_EVENT_REPORTED, "", handle,
       HCI_READ_ENCR_KEY_SIZE, HCI_COMMAND_COMPLETE_EVT, status, 0, key_size);
   if (ret < 0) {
     LOG(WARNING) << __func__ << ": failed to log encryption key size "
